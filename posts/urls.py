91c7f8024506b142b4fe98de5660aba43ea9f279
EDUARDO SALAZAR <eduardosalazar@Ed-2.local>
2016-09-05 08:17:04 -0500

    Routes Post

--- posts/urls.py
+++ posts/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-from posts.views import post_home
+from posts import views
+
 
 urlpatterns = [
-    url(r'^$', post_home),
+    url(r'^$', views.post_list),
+    url(r'^create/$', views.post_create),
+    url(r'^detail/$', views.post_detail),
+    url(r'^update/$', views.post_update),
+    url(r'^delete/$', views.post_delete),
+
 ]
--- posts/views.py
+++ posts/views.py
@@ -4,5 +4,30 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def post_home(request):
-    return HttpResponse("<h1>Hello</h1>")
+def post_create(request):
+    return HttpResponse("<h1>Create</h1>")
+
+
+def post_detail(request):
+    return HttpResponse("<h1>Detail</h1>")
+
+
+def post_update(request):
+    return HttpResponse("<h1>Update</h1>")
+
+
+def post_delete(request):
+    return HttpResponse("<h1>Delete</h1>")
+
+
+def post_list(request):
+    if request.user.is_authenticated():
+        content = {
+            "title": "List Content Auth"
+        }
+    else:
+        content = {
+            "title": "List Content Auth!"
+        }
+
+    return render(request, "index.html", content)
