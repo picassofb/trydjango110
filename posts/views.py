19e5fc854afcebf171c45c18371210c600de8e13
Eduardo Salazar <picassofb@hotmail.com>
2016-09-09 21:59:51 +0000

    Create Posts / List Posts

--- posts/views.py
+++ posts/views.py
@@ -1,25 +1,36 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
+from forms import PostForm
 
 # Create your views here.
 from posts.models import Post
 
 def post_create(request):
-    return HttpResponse("<h1>Create</h1>")
+    form = PostForm(request.POST or None)
+    
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+    
+    #if request.method == "POST":
+    #   print request.POST.get("title")
+    
+    context = {
+        "form": form
+    }
+    
+    return render(request, "post_form.html", context)
 
 
-def post_detail(request):
+def post_detail(request, id=None):
     #queryset = Post.objects.filter(id=6).values()
-    queryset = get_object_or_404(Post,id=3)
+    queryset = get_object_or_404(Post,id=id)
         
     content = {
         "instance": queryset, 
-     }
+    }
     
-
     return render(request, "post_detail.html", content)
-
-
 
 def post_update(request):
     return HttpResponse("<h1>Update</h1>")
