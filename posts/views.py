6f2568f6228400bde3992cc2797c5c0cf4e3a068
Ed <picassofb@hotmail.com>
2016-09-25 20:25:21 -0500

    Add slug flied / url rewriting according slug

--- posts/views.py
+++ posts/views.py
@@ -25,8 +25,8 @@ def post_create(request):
     return render(request, "post_form.html", context)
 
 
-def post_update(request, id=None):
-    instance = get_object_or_404(Post, id=id)
+def post_update(request, slug=None):
+    instance = get_object_or_404(Post, slug=slug)
     form = PostForm(request.POST or None, request.FILES or None, instance=instance)
 
     if form.is_valid():
@@ -42,8 +42,8 @@ def post_update(request, id=None):
     return render(request, "post_form.html", context)
 
 
-def post_detail(request, id=None):
-    instance = get_object_or_404(Post, id=id)
+def post_detail(request, slug=None):
+    instance = get_object_or_404(Post, slug=slug)
     context = {
         "title": instance.title,
         "instance": instance
@@ -51,8 +51,8 @@ def post_detail(request, id=None):
     return render(request, "post_detail.html", context)
 
 
-def post_delete(request, id=None):
-    instance = get_object_or_404(Post, id=id)
+def post_delete(request, slug=None):
+    instance = get_object_or_404(Post, slug=slug)
     instance.delete()
     messages.success(request, "Post Deleted!")
     return redirect("posts:list")
