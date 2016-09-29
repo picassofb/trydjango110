ebbf9fcbb7a695b68412cd5fe6e720e60536f55f
Ed <picassofb@hotmail.com>
2016-09-28 21:12:01 -0500

    Facebook Comments / template

--- posts/models.py
+++ posts/models.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 
+from django.contrib.auth.models import User
 from django.db import models
 from django.db.models.signals import pre_save
 from django.core.urlresolvers import reverse
@@ -13,6 +14,7 @@ def upload_location(instance, filename):
 
 
 class Post(models.Model):
+    user = models.ForeignKey(User, default=1)
     title = models.CharField(max_length=20)
     slug = models.SlugField(unique=True)
     image = models.ImageField(upload_to=upload_location, blank=True,
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,6 @@
 from urllib.parse import quote_plus
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect, Http404
 from .forms import PostForm
 from django.contrib import messages
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
@@ -10,10 +10,14 @@ from posts.models import Post
 
 
 def post_create(request):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
+
     form = PostForm(request.POST or None, request.FILES or None)
     
     if form.is_valid():
         instance = form.save(commit=False)
+        instance.user = request.user
         instance.save()
         messages.success(request, "Successfully created")
         return HttpResponseRedirect(instance.get_absolute_url())
@@ -27,6 +31,9 @@ def post_create(request):
 
 
 def post_update(request, slug=None):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
+
     instance = get_object_or_404(Post, slug=slug)
     form = PostForm(request.POST or None, request.FILES or None, instance=instance)
 
@@ -55,6 +62,8 @@ def post_detail(request, slug=None):
 
 
 def post_delete(request, slug=None):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
     instance = get_object_or_404(Post, slug=slug)
     instance.delete()
     messages.success(request, "Post Deleted!")
